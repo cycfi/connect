83aed4d239b491e93b444aea3beccd4738a8c57f
djowel <djowel@gmail.com>
2025-08-23 05:21:50 +0800

    Added fab file

---
+++ mom_2way/fab/fab.sh
@@ -0,0 +1,15 @@
+kikit panelize \
+    --layout 'grid; rows: 4; cols: 4; space: 2mm' \
+    --tabs 'fixed; width: 5mm; vcount: 1; hcount: 0' \
+    --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: -0.251mm; prolong: -0.3mm' \
+    --post 'millradius: 1mm' \
+    --framing 'railstb; width: 5mm; space: 3mm;' \
+    --tooling '3hole; hoffset: 2.5mm; voffset: 2.5mm; size: 1.5mm' \
+    --fiducials '3fid; hoffset: 5mm; voffset: 2.5mm; coppersize: 2mm; opening: 1mm;' \
+    --text 'simple; text: JLCJLCJLCJLC; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;' \
+    --post 'millradius: 1mm' \
+    ../mom_2way.kicad_pcb mom_2way_panel.kicad_pcb
+
+kikit fab jlcpcb --no-drc --assembly --schematic ../mom_2way.kicad_sch mom_2way_panel.kicad_pcb .
+mv gerbers.zip mom_2way_gerbers.zip
+
